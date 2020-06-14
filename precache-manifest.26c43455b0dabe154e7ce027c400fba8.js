b8583c1d2f1ae1a5bb1896611701a47387148644
Link1986 <ali.ghoudi@gmail.com>
2020-06-14 11:58:07 +0200

    Updates

---
+++ precache-manifest.26c43455b0dabe154e7ce027c400fba8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b482dc1b42ae2c23a81c0e9fa94bbd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6009aa6596c81f62def7",
+    "url": "/monsters-rolodex/static/css/main.976b2fd1.chunk.css"
+  },
+  {
+    "revision": "8eefb47c5e7c6f1166f6",
+    "url": "/monsters-rolodex/static/js/2.27033893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.27033893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6009aa6596c81f62def7",
+    "url": "/monsters-rolodex/static/js/main.c7916f8e.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
